e48649b92830dbb3e9d6ef2e690081d26b622d58
Matthew MacFarquhar <macfarqu@amazon.com>
2025-08-02 12:37:47 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use std::{io::{Read, Write}, net::TcpListener};
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}, thread};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,21 +9,27 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                loop {
-                    let mut buf = [0; 512];
-                    let read_count = stream.read(&mut buf).unwrap();
-                    if read_count == 0 {
-                        break;
-                    }
-
-                    stream.write_all(b"+PONG\r\n").unwrap();
-                }
+                thread::spawn(move || {
+                    handle_client(stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    loop {
+        let mut buf = [0; 512];
+        let read_count = stream.read(&mut buf).unwrap();
+        if read_count == 0 {
+            break;
+        }
+
+        stream.write_all(b"+PONG\r\n").unwrap();
     }
 }
