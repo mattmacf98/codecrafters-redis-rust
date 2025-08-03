1f7566bbb5174f24e7e53552d40b211d74443d93
Matthew MacFarquhar <macfarqu@amazon.com>
2025-08-03 11:20:45 -0400

    support expiry times

--- src/redis/client.rs
+++ src/redis/client.rs
@@ -2,8 +2,13 @@ use std::collections::HashMap;
 
 use crate::{redis::{create_bulk_string_resp, create_null_bulk_string_resp, create_simple_string_resp}, resp::types::RespType};
 
+
+struct CacheVal {
+    val: String,
+    expiry_time: Option<u128>
+}
 pub struct Client {
-    cache: HashMap<String, String>,
+    cache: HashMap<String, CacheVal>,
 }
 
 impl Client {
@@ -33,30 +38,89 @@ impl Client {
                         }
 
                         // SET
-                        if command.eq("set") && i < resp_types.len() - 2 {
-                            match (resp_types.get(i + 1).unwrap(), resp_types.get(i + 2).unwrap()) {
-                                (RespType::String(key), RespType::String(val)) => {
-                                    self.cache.insert(key.clone(), val.clone());
-                                    return Some(create_simple_string_resp("OK".to_string()))
-                                },
-                                (_, _) => panic!("INVALID SET COMMANDS")
-                            }
+                        if command.eq("set") {
+                            return self.handle_set(i, &resp_types);
                         }
 
-                        // GET
-                        if command.eq("get") && i < resp_types.len() - 1 {
-                            if let RespType::String(key) = resp_types.get(i + 1).unwrap() {
-                                let value = self.cache.get(key);
-                                return match value {
-                                    Some(v) => Some(create_simple_string_resp(v.to_string())),
-                                    None => Some(create_null_bulk_string_resp())
-                                }
-                            }
+                        if command.eq("get") {
+                            return self.handle_get(i, &resp_types);
                         } 
                     }
                 }
             },
             _ => panic!("unhandled type")
+        }
+
+        None
+    }
+
+    fn handle_set(&mut self, i: usize, resp_types: &Vec<RespType>) -> Option<String> {
+        if i + 4 < resp_types.len() {
+            match (resp_types.get(i + 1).unwrap(), resp_types.get(i + 2).unwrap(), resp_types.get(i + 3).unwrap(), resp_types.get(i + 4).unwrap()) {
+                (RespType::String(key), RespType::String(val), px, exp ) => {
+                    match (px, exp) {
+                        (RespType::String(px), RespType::String(exp)) => {
+                            if px.to_lowercase().eq("px") {
+                                if let Ok(exp_millis) = exp.parse::<u128>() {
+                                    let expiry = std::time::SystemTime::now()
+                                        .duration_since(std::time::UNIX_EPOCH)
+                                        .unwrap()
+                                        .as_millis() + exp_millis;
+                                    self.cache.insert(key.clone(), CacheVal { val: val.to_string(), expiry_time: Some(expiry) });
+                                    return Some(create_simple_string_resp("OK".to_string()))
+                                } else {
+                                    panic!("Invalid expiry time")
+                                }
+                            } else {
+                                self.cache.insert(key.clone(), CacheVal { val: val.to_string(), expiry_time: None });
+                                return Some(create_simple_string_resp("OK".to_string()))
+                            }
+                        }
+                        (_,_) => {
+                            self.cache.insert(key.clone(), CacheVal { val: val.to_string(), expiry_time: None });
+                            return Some(create_simple_string_resp("OK".to_string()))
+                        }
+                    }
+                },
+                (_, _, _ ,_) => panic!("INVALID SET COMMANDS")
+            }
+        } else if i + 2 < resp_types.len() {
+            match (resp_types.get(i + 1).unwrap(), resp_types.get(i + 2).unwrap()) {
+                (RespType::String(key), RespType::String(val)) => {
+                    self.cache.insert(key.clone(), CacheVal { val: val.to_string(), expiry_time: None });
+                    return Some(create_simple_string_resp("OK".to_string()))
+                },
+                (_, _) => panic!("INVALID SET COMMANDS")
+            }
+        }
+
+        None
+    }
+
+    fn handle_get(&self, i: usize, resp_types: &Vec<RespType>) -> Option<String> {
+        if i + 1 < resp_types.len() {
+            if let RespType::String(key) = resp_types.get(i + 1).unwrap() {
+                let value = self.cache.get(key);
+                return match value {
+                    Some(v) =>  {
+                        match v.expiry_time {
+                            Some(exp) => {
+                                let now = std::time::SystemTime::now()
+                                    .duration_since(std::time::UNIX_EPOCH)
+                                    .unwrap()
+                                    .as_millis();
+                                if now < exp {
+                                    Some(create_simple_string_resp(v.val.to_string()))
+                                } else {
+                                    Some(create_null_bulk_string_resp())
+                                }
+                            },
+                            None => Some(create_simple_string_resp(v.val.to_string()))
+                        }
+                    }
+                    None => Some(create_null_bulk_string_resp())
+                }
+            }
         }
 
         None
@@ -82,7 +146,32 @@ mod tests {
         let value = res.unwrap();
         assert!(value.eq("+OK\r\n"));
         assert!(client.cache.contains_key("foo"));
-        assert!(client.cache.get("foo").unwrap().eq("bar"))
+        let cache_val = client.cache.get("foo").unwrap();
+        assert!(cache_val.val.eq("bar"));
+        assert!(cache_val.expiry_time.is_none());
+    }
+
+    #[test]
+    fn test_set_with_expr_command() {
+        let cmds = vec![
+            RespType::String("SET".to_string()),
+            RespType::String("foo".to_string()),
+            RespType::String("bar".to_string()),
+            RespType::String("PX".to_string()),
+            RespType::String("100".to_string()),
+        ];
+        let cmd = RespType::Array(cmds);
+
+        let mut client = Client::new();
+        let res = client.handle_command(cmd);
+        assert!(res.is_some());
+        let value = res.unwrap();
+        assert!(value.eq("+OK\r\n"));
+        assert!(client.cache.contains_key("foo"));
+        let cache_val = client.cache.get("foo").unwrap();
+        assert!(cache_val.val.eq("bar"));
+        assert!(cache_val.expiry_time.is_some());
+        assert!(cache_val.expiry_time.unwrap() > 100);
     }
 
     #[test]
@@ -94,7 +183,44 @@ mod tests {
         let cmd = RespType::Array(cmds);
 
         let mut client = Client::new();
-        client.cache.insert("foo".to_string(), "bar".to_string());
+        client.cache.insert("foo".to_string(), CacheVal { val: "bar".to_string(), expiry_time: None });
+        let res = client.handle_command(cmd);
+        assert!(res.is_some());
+        let value = res.unwrap();
+        assert!(value.eq("+bar\r\n"));
+    }
+
+    #[test]
+    fn test_get_expired_command() {
+        let cmds = vec![
+            RespType::String("GET".to_string()),
+            RespType::String("foo".to_string())
+        ];
+        let cmd = RespType::Array(cmds);
+
+        let mut client = Client::new();
+        client.cache.insert("foo".to_string(), CacheVal { val: "bar".to_string(), expiry_time: Some(500) });
+        let res = client.handle_command(cmd);
+        assert!(res.is_some());
+        let value = res.unwrap();
+        assert!(value.eq("$-1\r\n"));
+    }
+
+    #[test]
+    fn test_get_non_expired_command() {
+        let cmds = vec![
+            RespType::String("GET".to_string()),
+            RespType::String("foo".to_string())
+        ];
+        let cmd = RespType::Array(cmds);
+
+        let now = std::time::SystemTime::now()
+                                    .duration_since(std::time::UNIX_EPOCH)
+                                    .unwrap()
+                                    .as_millis();
+
+        let mut client = Client::new();
+        client.cache.insert("foo".to_string(), CacheVal { val: "bar".to_string(), expiry_time: Some(now + 60000) });
         let res = client.handle_command(cmd);
         assert!(res.is_some());
         let value = res.unwrap();
