c0a3fd14686790473e69f858ba9597e2c0138b65
Matthew MacFarquhar <macfarqu@amazon.com>
2025-08-06 15:22:50 -0400

    enable replicant to execute commands sent to it

--- src/redis/instance.rs
+++ src/redis/instance.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, io::{Read, Write}, net::{TcpListener, TcpStream}, sync::{Arc, Mutex}, thread};
 
-use crate::{redis::{client::{CacheVal, Client}, create_array_resp, create_bulk_string_resp}, resp::types::RespType};
+use crate::{redis::{client::{self, CacheVal, Client}, create_array_resp, create_bulk_string_resp}, resp::types::RespType};
 pub struct Instance {
     is_master: bool,
     replica_of: Option<String>,
@@ -76,5 +76,35 @@ impl Instance {
         let mut response_buf = [0; 512];
         master_stream.read(&mut response_buf).unwrap();
         println!("Received from master: {}", String::from_utf8_lossy(&response_buf));
+
+        //deump RDB
+        let mut response_buf = [0; 512];
+        master_stream.read(&mut response_buf).unwrap();
+
+        let client = Client::new(self.cache.clone(), self.write_commands.clone(), self.replica_streams.clone(), self.replica_of.clone());
+        thread::spawn(move || {
+            Self::handle_master_connection(master_stream, client);
+        });
+    }
+
+    fn handle_master_connection(mut stream: TcpStream, mut client: Client) {
+        loop {
+            println!("LOOP");
+
+            let mut buf = [0; 512];
+            let read_count = stream.read(&mut buf).unwrap();
+            if read_count == 0 {
+                break;
+            }
+            let buffer = bytes::BytesMut::from(&buf[..read_count]);
+            println!("received: {}", String::from_utf8_lossy(&buffer));
+            let resp_res = RespType::parse(&buffer, 0);
+            match resp_res {
+                Ok(res) => {
+                    client.handle_command(res.0);
+                },
+                Err(e) => panic!("ERROR {:?}", e),
+            };
+        }
     }
 }
