7928be976eb7185e46fe09e9b52965e986c778f9
Matthew MacFarquhar <macfarqu@amazon.com>
2025-08-08 11:37:11 -0400

    clean up rdb parsing

--- README.md
+++ README.md
@@ -8,5 +8,3 @@ Current Status: Completed all live modules as of 8/10/2025
 #TODO
 1. A way to do some of the network test locally for multi client interactions / replica interactions.
 2. there are lots of parameters going into instacne and client (can I encapsulate some of these into sub structs?)
-3. more logical directory structure (i.e. creating resp responses should be in resp module)
-4. rdb logic for parsing is very procedural, think about better structure functionally
--- src/rdb/rdb.rs
+++ src/rdb/rdb.rs
@@ -6,7 +6,7 @@ use crate::redis::client::{CacheVal, StringCacheVal};
 
 pub struct Rdb {
     version: String,
-    metadata: String,
+    metadata: HashMap<String, String>,
     key_values: Vec<KeyValue>,
 }
 
@@ -20,7 +20,13 @@ impl Rdb {
     pub fn new(rdb_data: BytesMut) -> Self {
         let (version, pos) = Self::extract_version(&rdb_data, 0);
         let (metadata, pos) = Self::extarct_metadata_section(&rdb_data, pos);
-        let (key_values, pos) = Self::extract_key_values(&rdb_data, pos);
+
+        if rdb_data[pos] == 0xFF {
+            println!("No keys");
+            return Self { version: version, metadata: metadata, key_values: Vec::new() };
+        }
+
+        let (key_values, pos) = Self::extract_key_values(&rdb_data, pos + 1);
         Self { version: version, metadata: metadata, key_values: key_values }
     }
 
@@ -31,132 +37,152 @@ impl Rdb {
         }
     }
 
-    fn extract_version(rdb_data: &BytesMut, pos: usize) -> (String, usize) {
-        // First 5 bytes should be "REDIS"
-        if  &rdb_data[pos..pos+5] != b"REDIS" {
-            panic!("Invalid RDB file format");
-        }
-
-        let mut version_bytes = vec![];
-        let mut current_pos = pos + 5;
-        loop {
-            let byte = rdb_data[current_pos];
-            current_pos += 1;
-            match byte {
-                0xFA => {
-                    // 0xFA is the end of the version string
-                    break;
-                }
-                _ => {
-                    version_bytes.push(byte);
-                }
-            }
-        }
-
-        let version_str = std::str::from_utf8(&version_bytes).expect("Version should be valid UTF-8");
-        println!("Version: {}", version_str);
-        return (version_str.to_string(), current_pos);
+    fn read_string(rdb_data: &BytesMut, pos: usize, bytes: usize) -> String {
+        String::from_utf8_lossy(&rdb_data[pos..pos+bytes]).to_string()
     }
 
-    fn extarct_metadata_section(rdb_data: &BytesMut, pos: usize) -> (String, usize) {
-        let mut metadata_bytes = vec![];
-        let mut current_pos = pos;
+    fn extract_string(rdb_data: &BytesMut, pos: usize) -> (String, usize) {
+        let mut cur_pos = pos;
+        let type_byte = rdb_data[cur_pos];
+        if type_byte & 0xc0 == 0xc0 {
+            let bytes_len = type_byte & !0xc0;
+            match bytes_len {
+                0 => {
+                    // 1 byte
+                    let byte = i8::from_le_bytes(rdb_data[cur_pos + 1..cur_pos + 2].try_into().unwrap());
+                    return (byte.to_string(), cur_pos + 2);
+                }
+                1 => {
+                    // 2 bytes
+                    let byte = i16::from_le_bytes(rdb_data[cur_pos + 1..cur_pos + 3].try_into().unwrap());
+                    return (byte.to_string(), cur_pos + 3);
+                }
+                2 => {
+                    // 4 bytes
+                    let byte = i32::from_le_bytes(rdb_data[cur_pos + 1..cur_pos + 5].try_into().unwrap());
+                    return (byte.to_string(), cur_pos + 5);
+                },
+                _ => panic!("Invalid string type byte: {}", type_byte)
+            }
+        } else {
+            // length-prefixed string
+            let str_len = rdb_data[cur_pos] as usize;
+            cur_pos += 1;
+            return (Self::read_string(rdb_data, cur_pos, str_len), cur_pos + str_len);
+        }
+    }
+
+    fn extract_version(rdb_data: &BytesMut, pos: usize) -> (String, usize) {
+        let mut cur_pos = pos;
+        // First 5 bytes should be "REDIS"
+        let magic = Self::read_string(rdb_data, cur_pos, 5);
+        if  magic.ne("REDIS") {
+            panic!("Invalid RDB file format");
+        }
+        cur_pos += 5;
+
+        let version_str = Self::read_string(rdb_data, cur_pos, 4);
+        println!("Version: {}", version_str);
+        cur_pos += 4;
+
+        return (version_str.to_string(), cur_pos);
+    }
+
+    fn extarct_metadata_section(rdb_data: &BytesMut, pos: usize) -> (HashMap<String, String>, usize) {
+        let mut cur_pos = pos;
+        assert!(rdb_data[cur_pos] == 0xFA);
+        cur_pos += 1;
+        
+        let mut metadata: HashMap<String, String> = HashMap::new();
+
         loop {
-            let byte = rdb_data[current_pos];
-            current_pos += 1;
+            let byte = rdb_data[cur_pos];
             match byte {
-                0xFE => {
+                0xFA => {
+                    // metadata sep, just eat
+                    cur_pos += 1;
+                    continue;
+                }
+                0xFE | 0xFF => {
                     break;
                 }
                 _ => {
-                    metadata_bytes.push(byte);
+                    let (key, new_pos) = Self::extract_string(rdb_data, cur_pos);
+                    cur_pos = new_pos;
+                    let (val, new_pos) = Self::extract_string(rdb_data, cur_pos);
+                    cur_pos = new_pos;
+                    metadata.insert(key, val);
                 }
             }
         }
-        let metadata_str = String::from_utf8_lossy(&metadata_bytes);
-        println!("Metadata: {}", metadata_str);
-        return (metadata_str.to_string(), current_pos);
+        println!("Metadata: {:?}", metadata);
+        return (metadata, cur_pos);
     }
 
     fn extract_key_values(rdb_data: &BytesMut, pos: usize) -> (Vec<KeyValue>, usize) {
-        let mut current_pos = pos;
-        let db_index = rdb_data[current_pos] as usize;
+        let mut cur_pos = pos;
+        let db_index = rdb_data[cur_pos] as usize;
         println!("DB index: {}", db_index);
-        current_pos += 1;
+        cur_pos += 1;
 
-        if rdb_data[current_pos] == 0xFF {
-            println!("No keys");
-            return (Vec::new(), current_pos);
-        }
+        assert_eq!(rdb_data[cur_pos], 0xFB);
+        cur_pos += 1;
 
-        assert_eq!(rdb_data[current_pos], 0xFB);
-        current_pos += 1;
-
-        let total_keys = rdb_data[current_pos] as usize;
+        let total_keys = rdb_data[cur_pos] as usize;
         println!("Total keys: {}", total_keys);
-        current_pos += 1;
+        cur_pos += 1;
 
-        let expiring_keys = rdb_data[current_pos] as usize;
+        let expiring_keys = rdb_data[cur_pos] as usize;
         println!("Expiring keys: {}", expiring_keys);
-        current_pos += 1;
+        cur_pos += 1;
 
 
         let mut key_values = Vec::new();
         for _ in 0..total_keys {
             let mut key_expiry_time = None;
-            match rdb_data[current_pos] {
+            match rdb_data[cur_pos] {
                 0xFD => {
                     // OxFD is the timestamp in seconds, eat 4 bytes
-                    let seconds_bytes = &rdb_data[current_pos+1..current_pos+5];
+                    let seconds_bytes = &rdb_data[cur_pos+1..cur_pos+5];
                     let seconds = u32::from_le_bytes(seconds_bytes.try_into().unwrap()) as u128;
                     key_expiry_time = Some(seconds);
                     println!("Key expiry time: {}", seconds);
-                    current_pos += 5;
+                    cur_pos += 5;
                 },
                 0xFC => {
                     // OxFC is the timestamp in milliseconds, eat 8 bytes
-                    let milliseconds_bytes = &rdb_data[current_pos+1..current_pos+9];
+                    let milliseconds_bytes = &rdb_data[cur_pos+1..cur_pos+9];
                     let milliseconds = u64::from_le_bytes(milliseconds_bytes.try_into().unwrap()) as u128;
                     key_expiry_time = Some(milliseconds);
                     println!("Key expiry time: {}", milliseconds);
-                    current_pos += 9;
+                    cur_pos += 9;
                 },
                 _ => {
                     // do nothing
                 }
             }
 
-            let val_type = rdb_data[current_pos] as usize;
-            println!("Value type: {}", val_type);
-            current_pos += 1;
+            let val_type = rdb_data[cur_pos] as usize;
+            cur_pos += 1;
 
-            //ASSUME STRING TYPE
-            let key_length = rdb_data[current_pos] as usize;
-            println!("Key length: {}", key_length);
-            current_pos += 1;
-
-            let mut key_bytes = vec![];
-            for _ in 0..key_length {
-             key_bytes.push(rdb_data[current_pos]);
-             current_pos += 1;
-            }
-            let key_str = String::from_utf8_lossy(&key_bytes);
-            println!("Key: {}", key_str);
-
-            let value_length = rdb_data[current_pos] as usize;
-            println!("Value length: {}", value_length);
-            current_pos += 1;
-
-            let mut value_bytes = vec![];
-            for _ in 0..value_length {
-             value_bytes.push(rdb_data[current_pos]);
-             current_pos += 1;
-            }
-            let value_str = String::from_utf8_lossy(&value_bytes);
-            println!("Value: {}", value_str);
-            key_values.push(KeyValue { key: key_str.to_string(), value: value_str.to_string(), expiry_time: key_expiry_time });
+            let (key, new_pos) = Self::extract_string(rdb_data, cur_pos);
+            cur_pos = new_pos;
+            let (val, new_pos) = Self::extract_string(rdb_data, cur_pos);
+            cur_pos = new_pos;
+            key_values.push(KeyValue { key: key, value: val, expiry_time: key_expiry_time });
         }
 
-        return (key_values, current_pos);
+        return (key_values, cur_pos);
+    }
+}
+
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty_rdb() {
+        let rdb_data = std::fs::read("empty.rdb").unwrap();
+        let rdb = Rdb::new(BytesMut::from(&rdb_data[..]));
+        assert_eq!(rdb.version, "0011");
     }
 }
